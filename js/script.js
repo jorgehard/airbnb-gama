56d19c6010939fb2548676aac57649d6d86d9c5d
Jorge Henrique <jorgehenrique@live.com>
2020-05-09 20:03:32 -0300

    Alteração no layout e integração da aplicação JS

--- js/script.js
+++ js/script.js
@@ -8,22 +8,49 @@ async function buscarApi(val1) {
 //Mostrando API
 function mostrarDados(data) {
     const total_result = Object.keys(data).length;
+    document.getElementById("totalJs").innerHTML = total_result;
     data.map(elemento => {
-        const root = document.getElementById("root");
+        const root = document.getElementById("apiBox");
         const { photo, name, property_type, price } = elemento;
-        // div = document.createElement("div");
 
-        // image = document.createElement("img");
-        // image.setAttribute("src", photo);
+        let div = document.createElement("div");
+        div.setAttribute("class", "col-4");
+        div.addEventListener("click", pageDados);
 
-        // pro = document.createElement("p");
-        // pro.innerHTML = property_type;
-        // price.innerHTML = `R$ ${price},00 `;
+        let boxImg = document.createElement("div");
+        boxImg.setAttribute("class", "boxImg");
 
-        // root.appendChild(div);
-        // div.appendChild(image);
+        let image = document.createElement("img");
+        image.setAttribute("src", photo);
+
+        let boxTitle = document.createElement("div");
+        boxTitle.setAttribute("class", "boxTitle");
+
+        let propertyType = document.createElement("p");
+        propertyType.setAttribute("class", "propertyType");
+        propertyType.innerHTML = property_type;
+
+        let nameType = document.createElement("p");
+        nameType.setAttribute("class", "nameType");
+        nameType.innerHTML = name;
+
+        let priceType = document.createElement("p");
+        priceType.setAttribute("class", "price");
+        priceType.innerHTML = `<b>R$ ${price},00</b> / diária`;
+
+        root.appendChild(div);
+        div.appendChild(boxImg);
+        boxImg.appendChild(image);
+        boxImg.appendChild(boxTitle);
+        boxTitle.appendChild(propertyType);
+        boxTitle.appendChild(nameType);
+        boxTitle.appendChild(priceType);
     });
 }
+function pageDados(data) {
+    console.log("ok");
+}
 
+// Chamando API Inicial
 buscarApi(api_url)
     .then((mostrarDados))
